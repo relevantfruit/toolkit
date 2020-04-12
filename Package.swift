f4bc5cdab1c0c3c3280a59f8542c2caa9a13e1da
Yagiz Nizipli <yagiznizipli@me.com>
2020-04-12 04:09:45 +0300

    fix: update dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   products: [
     .library(name: "Toolkit", targets: ["Toolkit"]),
-    .library(name: "ToolkitRxSwift", type: .dynamic, targets: ["ToolkitRxSwift"])
+    .library(name: "ToolkitRxSwift", targets: ["ToolkitRxSwift"])
   ],
   dependencies: [
     .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1"),
@@ -20,17 +20,22 @@ let package = Package(
     .package(url: "https://github.com/evgenyneu/keychain-swift", from: "19.0.0"),
     .package(url: "https://github.com/bizz84/SwiftyStoreKit", from: "0.15.0"),
     .package(url: "https://github.com/marmelroy/Localize-Swift", from: "3.1.0"),
-    .package(url: "https://github.com/huri000/SwiftEntryKit", from: "1.2.3"),
     .package(url: "https://github.com/sunshinejr/Moya-ModelMapper", .branch("master")),
     .package(url: "https://github.com/AliSoftware/Reusable", from: "4.1.1")
   ],
   targets: [
     .target(
       name: "Toolkit",
-      dependencies: ["SnapKit", "Lottie", "DeviceKit", "KeychainSwift", "SwiftyStoreKit", "Localize_Swift", "SwiftEntryKit", "Reusable"], path: "Sources", exclude: ["RxSwift"]),
+      dependencies: [
+        "SnapKit", "Lottie", "DeviceKit", "KeychainSwift", "SwiftyStoreKit", "Localize_Swift", "Reusable"
+      ],
+      path: "Sources",
+      exclude: ["RxSwift"]),
     .target(
       name: "ToolkitRxSwift",
-      dependencies: ["Toolkit", "RxMoya-ModelMapper"], path: "Sources", sources: ["RxSwift"])
+      dependencies: ["RxMoya-ModelMapper"],
+      path: "Sources",
+      sources: ["RxSwift"])
   ],
   swiftLanguageVersions: [.v5]
 )
