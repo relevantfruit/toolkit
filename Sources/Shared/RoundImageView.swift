90d3852bb684dfdd3eaec672f730aef80b917dc8
Yagiz Nizipli <yagiznizipli@me.com>
2020-07-16 15:56:14 +0300

    fix(dependencies): update dependencies and remove macOS support

--- Sources/Shared/RoundImageView.swift
+++ Sources/Shared/RoundImageView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-public class RoundImageView: UIImageView {
+public final class RoundImageView: UIImageView {
   public override func layoutSubviews() {
     super.layoutSubviews()
     self.layer.cornerRadius = self.bounds.size.width / 2.0
