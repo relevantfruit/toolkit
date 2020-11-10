c0898f9cc51b8b03696eed65767ae4fb51e02803
Yagiz Nizipli <yagiznizipli@me.com>
2020-11-10 16:10:13 +0300

    fix(dependencies): update dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let rxDependencies: [Package.Dependency] = [
-  .package(url: "https://github.com/rxswiftcommunity/rxflow", from: "2.9.0"),
+  .package(url: "https://github.com/rxswiftcommunity/rxflow", from: "2.11.0"),
   .package(url: "https://github.com/sunshinejr/Moya-ModelMapper", from: "10.0.0"),
   .package(url: "https://github.com/RxSwiftCommunity/RxRealm", from: "3.1.0"),
   .package(url: "https://github.com/RxSwiftCommunity/RxGesture", from: "3.0.3")
@@ -14,8 +14,8 @@ let package = Package(
   name: "Toolkit",
   platforms: [
     .iOS(.v12),
-    .tvOS(.v10),
-    .watchOS(.v3)
+    .tvOS(.v12),
+    .watchOS(.v5)
   ],
   products: [
     .library(name: "Toolkit", targets: ["Toolkit"]),
@@ -23,11 +23,11 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1"),
-    .package(url: "https://github.com/airbnb/lottie-ios", from: "3.1.8"),
-    .package(url: "https://github.com/devicekit/DeviceKit", from: "3.2.0"),
+    .package(url: "https://github.com/airbnb/lottie-ios", from: "3.1.9"),
+    .package(url: "https://github.com/devicekit/DeviceKit", from: "4.2.1"),
     .package(url: "https://github.com/evgenyneu/keychain-swift", from: "19.0.0"),
     .package(url: "https://github.com/bizz84/SwiftyStoreKit", from: "0.16.1"),
-    .package(url: "https://github.com/marmelroy/Localize-Swift", from: "3.1.0"),
+    .package(url: "https://github.com/marmelroy/Localize-Swift", from: "3.2.0"),
     .package(url: "https://github.com/AliSoftware/Reusable", from: "4.1.1")
   ] + rxDependencies,
   targets: [
